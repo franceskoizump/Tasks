4751e2df3d69573607580842ca1b1602e2bff4fc
Иван Петряков <franceskoizump@gmail.com>
2018-10-13 22:19:23 +0300

    wrong pref sum solution

--- src/cl/max_prefix_sum.cl
+++ src/cl/max_prefix_sum.cl
@@ -1,1 +1,78 @@
 // TODO
+
+__kernel void sum_blocks(__global int * in, unsigned int n, __local int * buffer)
+{
+    unsigned int local_id = get_local_id(0);
+    unsigned int global_id = get_global_id(0);
+    unsigned int local_size = get_local_size(0);
+    if (n <= global_id) {
+        buffer[local_id] = 0;
+    }
+    else { 
+        buffer[local_id] = in[global_id];
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+    for (int i = 0; i < 7; i+=1)//7 because of log(local_size(0)) == 7
+    {
+        unsigned int off = (1 << i);
+        if (local_id >= off)
+        {
+            buffer[local_id] = buffer[local_id - off] + buffer[local_id];
+        }
+    barrier(CLK_LOCAL_MEM_FENCE);
+    }
+    in[global_id] = buffer[local_id];
+    
+}
+
+__kernel void add(__global int * in, unsigned int n, __local int * buffer)
+{
+    unsigned int global_id = get_global_id(0);
+    unsigned int group_id = get_group_id(0);
+    unsigned int local_size = get_local_size(0);
+    unsigned int local_id = get_local_id(0);
+    if (n <= global_id) {
+        buffer[local_id] = 0;
+    }
+    else { 
+        buffer[local_id] = in[global_id];
+    }
+    for (int i = 0; i < group_id; i++)
+    {
+        buffer[local_id] += in[(i+1) * local_size - 1];
+    }
+    barrier(CLK_GLOBAL_MEM_FENCE);
+    in[global_id] = buffer[local_id];
+}
+
+
+__kernel void max_prefix_sum(__global unsigned int * in,
+                  __global unsigned int * result,
+                  __global unsigned int * index,
+                   unsigned int n,
+                  __local  unsigned int * buffer, __local unsigned int * ind_b)
+
+{
+    int local_id = get_local_id(0);
+    unsigned int global_id = get_global_id(0);
+    int local_size = get_local_size(0);
+    if (global_id >= n) buffer[local_id] = -1000;
+    else buffer[local_id] = in[global_id];
+    barrier(CLK_LOCAL_MEM_FENCE);
+    for (int i = local_size; i > 1; i/=2)
+    {
+        if (2 * local_id < i && global_id < n)
+        {
+            unsigned int a = buffer[local_id];
+            unsigned int b = buffer[local_id + i /2];
+            buffer[local_id] = a < b ? b : a;
+            ind_b[local_id] = a < b ? local_id + i/2 : local_id ;
+
+        }
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+    if (local_id == 0)
+    {
+        atom_max(result, buffer[0]);
+    }
+}
--- src/main_max_prefix_sum.cpp
+++ src/main_max_prefix_sum.cpp
@@ -2,6 +2,13 @@
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
 
+#include <libutils/misc.h>
+#include <libutils/timer.h>
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
+
+#include "cl/max_prefix_sum_cl.h"
+
 
 template<typename T>
 void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
@@ -72,6 +79,64 @@ int main(int argc, char **argv)
 
         {
             // TODO: implement on OpenCL
+            gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+            gpu::Context context;
+            context.init(device.device_id_opencl);
+            context.activate();
+            ocl::Kernel kernel_sum(max_prefix_sum_kernel, max_prefix_sum_kernel_length, "sum_blocks");
+            ocl::Kernel kernel_add(max_prefix_sum_kernel, max_prefix_sum_kernel_length, "add");
+            ocl::Kernel kernel_max(max_prefix_sum_kernel, max_prefix_sum_kernel_length, "max_prefix_sum");
+            kernel_sum.compile();
+            kernel_add.compile();
+            kernel_max.compile();
+            gpu::gpu_mem_32i as_gpu;
+            gpu::gpu_mem_32i res;
+            gpu::gpu_mem_32i ind;
+        
+        
+            as_gpu.resizeN(n);
+            as_gpu.writeN(as.data(), n);
+            unsigned int workGroupSize = 128;
+            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
+            res.resizeN(1);
+            ind.resizeN(1);
+            ocl::LocalMem buffer(sizeof(int) * workGroupSize);
+            ocl::LocalMem buf_id(sizeof(int) * workGroupSize);
+            timer t;
+            std::vector<int> a = as;
+            std::vector<int> r = as;
+            for (int i = 1; i < a.size(); i++)
+            {
+                a[i] = a[i] + a[i-1];
+            }
+            for (int iter = 0; iter < benchmarkingIters; ++iter) {
+                int pref = -100000;
+                int index = 0;
+                res.writeN(&pref, 1);
+                ind.writeN(&index, 1);
+                kernel_sum.exec(gpu::WorkSize(workGroupSize, global_work_size),
+                        as_gpu, n, buffer);
+            
+                
+                std::cout << "WOW" << std::endl;
+                kernel_add.exec(gpu::WorkSize(workGroupSize, global_work_size),
+                        as_gpu, n, buffer);
+                as_gpu.readN(r.data(), n);
+              std::cout << n << std::endl;    
+                for (int i = 0; i < n; i++)
+            {
+                std::cout << a[i] << "   ==== " << r[i] << std::endl;
+            }
+                kernel_max.exec(gpu::WorkSize(workGroupSize, global_work_size),
+                        as_gpu, res, ind, n, buffer, buf_id);
+                res.readN(&pref, 1);
+                ind.readN(&index, 1);
+                EXPECT_THE_SAME(pref, reference_max_sum, "GPU result should be consistent!");
+                EXPECT_THE_SAME(index, reference_result, "GPU result should be consistent!");
+                t.nextLap();
+            }
+            std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+            std::cout << "GPU: " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
         }
     }
 }
